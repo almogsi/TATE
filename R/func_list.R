1f2f1c7fdb6435c63cab132e43fcfd9f1ba01901
Almog Simchon <almog.si@gmail.com>
2018-11-22 15:09:19 +0200

    Add files via upload

--- R/func_list.R
+++ R/func_list.R
@@ -5,9 +5,9 @@ NoVAD <- function(text){
   b <- tolower(as.character(text))
   b <-  textstem::lemmatize_strings(b)
   which.txt <- which(NoVAD_txt$Word %in% unlist(stringr::str_split(b, " ")))
-  valence <- sum(NoVAD_txt$V.Mean.Sum[which.txt]) / length(which.txt)
-  arousal <- sum(NoVAD_txt$A.Mean.Sum[which.txt]) / length(which.txt)
-  dominance <- sum(NoVAD_txt$D.Mean.Sum[which.txt]) / length(which.txt)
+  valence <- round(sum(NoVAD_txt$V.Mean.Sum[which.txt]) / length(which.txt), 2)
+  arousal <- round(sum(NoVAD_txt$A.Mean.Sum[which.txt]) / length(which.txt),2)
+  dominance <- round(sum(NoVAD_txt$D.Mean.Sum[which.txt]) / length(which.txt),2)
   return(c(Valence = valence, Arousal = arousal, Dominance = dominance))
 }
 
@@ -16,7 +16,7 @@ concretness <- function(text){
   b <- tolower(as.character(text))
   b <-  textstem::lemmatize_strings(b)
   which.txt <- which(conc$Word %in% unlist(stringr::str_split(b, " ")))
-  concretness <- sum(conc$Conc.M[which.txt]) / length(which.txt)
+  concretness <- round(sum(conc$Conc.M[which.txt]) / length(which.txt),2)
   return(c(Concretness = concretness))
 }
 
@@ -25,7 +25,7 @@ funny <- function(text){
   b <- tolower(as.character(text))
   b <-  textstem::lemmatize_strings(b)
   which.txt <- which(humor$word %in% unlist(stringr::str_split(b, " ")))
-  funny <- sum(humor$mean[which.txt]) / length(which.txt)
+  funny <- round(sum(humor$mean[which.txt]) / length(which.txt),2)
   return(c(funny = funny))
 }
 
@@ -34,8 +34,8 @@ EvLex <- function(text){
   b <- tolower(as.character(text))
   b <-  textstem::lemmatize_strings(b)
   which.txt <- which(EL$Word %in% unlist(stringr::str_split(b, " ")))
-  Valence <- sum(EL$Valence[which.txt]) / length(which.txt)
-  Extremity <- sum(EL$Extremity[which.txt]) / length(which.txt)
-  Emotionality <- sum(EL$Emotionality[which.txt]) / length(which.txt)
+  Valence <- round(sum(EL$Valence[which.txt]) / length(which.txt),2)
+  Extremity <- round(sum(EL$Extremity[which.txt]) / length(which.txt),2)
+  Emotionality <- round(sum(EL$Emotionality[which.txt]) / length(which.txt),2)
   return(c(Valence = Valence, Extremity = Extremity, Emotionality = Emotionality))
 }
