7ddabacd4c7e4981ef4b9f7d63f7f4dc3070c892
Almog Simchon <almog.si@gmail.com>
2018-11-22 14:42:27 +0200

    Add files via upload

--- R/func_list.R
+++ R/func_list.R
@@ -4,7 +4,7 @@ data(.sysdata, envir=environment())
 NoVAD <- function(text){
   b <- tolower(as.character(text))
   b <-  textstem::lemmatize_strings(b)
-  which.txt <- which(stringr::str_detect(b, stringr::regex(paste0(NoVAD_txt$Word, "\\b"))))
+  which.txt <- which(NoVAD_txt$Word %in% unlist(stringr::str_split(b, " ")))
   valence <- sum(NoVAD_txt$V.Mean.Sum[which.txt]) / length(which.txt)
   arousal <- sum(NoVAD_txt$A.Mean.Sum[which.txt]) / length(which.txt)
   dominance <- sum(NoVAD_txt$D.Mean.Sum[which.txt]) / length(which.txt)
@@ -15,7 +15,7 @@ concretness <- function(text){
 
   b <- tolower(as.character(text))
   b <-  textstem::lemmatize_strings(b)
-  which.txt <- which(stringr::str_detect(b, stringr::regex(paste0(conc$Word, "\\b"))))
+  which.txt <- which(conc$Word %in% unlist(stringr::str_split(b, " ")))
   concretness <- sum(conc$Conc.M[which.txt]) / length(which.txt)
   return(c(Concretness = concretness))
 }
@@ -24,7 +24,7 @@ funny <- function(text){
 
   b <- tolower(as.character(text))
   b <-  textstem::lemmatize_strings(b)
-  which.txt <- which(stringr::str_detect(b, stringr::regex(paste0(humor$word, "\\b"))))
+  which.txt <- which(humor$word %in% unlist(stringr::str_split(b, " ")))
   funny <- sum(humor$mean[which.txt]) / length(which.txt)
   return(c(funny = funny))
 }
@@ -33,7 +33,7 @@ EvLex <- function(text){
 
   b <- tolower(as.character(text))
   b <-  textstem::lemmatize_strings(b)
-  which.txt <- which(stringr::str_detect(b, stringr::regex(paste0(EL$Word, "\\b"))))
+  which.txt <- which(EL$Word %in% unlist(stringr::str_split(b, " ")))
   Valence <- sum(EL$Valence[which.txt]) / length(which.txt)
   Extremity <- sum(EL$Extremity[which.txt]) / length(which.txt)
   Emotionality <- sum(EL$Emotionality[which.txt]) / length(which.txt)
